3f95acf35153af41261d22dd6d3da9b281015944
rafei18 <tchouar.rafei@gmail.com>
2024-01-30 11:54:27 +0100

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 import dill # helps us to create the pickele file 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 from exception import CustomException
@@ -29,12 +30,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            #para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            #gs = GridSearchCV(model,para,cv=3)
-            #gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            #model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             
 
             model.fit(X_train, y_train)  # Train model
